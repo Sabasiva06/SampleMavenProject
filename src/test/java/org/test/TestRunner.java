e832a2abb2591cf1c3df577f4f931e29e192d67d
Sabasiva06 <sabasiva06@gmail.com>
2020-12-18 09:54:50 +0530

    ABCD Updated CODE

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -1,9 +1,13 @@
 package org.test;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.sun.tools.xjc.Driver;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import groovyjarjarpicocli.CommandLine.Spec;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src\\test\\resources\\jira.feature" }, glue = {
@@ -11,5 +15,13 @@ import cucumber.api.junit.Cucumber;
 
 public class TestRunner {
 
+	@AfterClass
+	public void afterClass() {
+		System.out.println("update");
+		Driver.getBuildID();
+		System.out.println("clone");
+		System.out.println("update");
+		Driver.getBuildID();
+		System.out.println("clone");
+	}
 }
-
